de80e4eea41b41e549d87bf61c467100671a1963
mariavaal <vazquezalonso.maria@gmail.com>
2018-05-10 20:15:10 +0200

    [FIX] scat_school_management: Corregimos la relación entre expedientes y cliente

--- project-addons/scat_school_management/models/scat_expediente.py
+++ project-addons/scat_school_management/models/scat_expediente.py
@@ -7,7 +7,7 @@ class scat_expediente(models.Model):
     _name = "scat.expediente"
 
     n_expediente = fields.Integer(string="Nº de expediente", required= True)
-    client= fields.Char(string="Cliente")
+    partner_id= fields.Many2one('res.partner', 'Cliente', required=True)
     start_date = fields.Date(string="Fecha inicio", required=True)
     end_date = fields.Date(string="Fecha fin")
     school_ids = fields.Many2many('scat.school', string="Colegio/s")
