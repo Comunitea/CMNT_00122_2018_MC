dc883a59d200697fba70564355a07ccd50769939
Omar <omarcs7r@gmail.com>
2018-07-20 15:25:11 +0200

    [FIX] scat_school_management: La compañía en apuntes analíticos ya no es una related

--- project-addons/scat_school_management/models/scat_school.py
+++ project-addons/scat_school_management/models/scat_school.py
@@ -9,14 +9,29 @@ class scat_school(models.Model):
 
     code = fields.Char("Referencia", required=True)
     notes = fields.Text(string="Notas")
-    school_id = fields.Many2one('project.project', string="Colegio", required=True, ondelete="cascade")
-    state_id = fields.Many2one("res.country.state", related="school_id.partner_id.state_id", readonly=True)
-    expedient_ids = fields.Many2many(comodel_name='scat.expediente',
-        relation='scat_expediente_scat_school_rel',
-        column1='scat_school_id', column2='scat_expediente_id', string="Expediente/s")
+    school_id = fields.Many2one('project.project', string="Colegio",
+                                required=True, ondelete="cascade")
+    state_id = fields.Many2one("res.country.state",
+                               related="school_id.partner_id.state_id",
+                               readonly=True)
+    expedient_ids = fields.\
+        Many2many(comodel_name='scat.expediente',
+                  relation='scat_expediente_scat_school_rel',
+                  column1='scat_school_id', column2='scat_expediente_id',
+                  string="Expediente/s")
 
 
 class AccountAnalyticAccount(models.Model):
-    _inherit="account.analytic.account"
 
-    company_id=fields.Many2one('res.company', required=False)
+    _inherit = "account.analytic.account"
+
+    company_id = fields.Many2one('res.company', required=False)
+
+
+class AccountAnalyticLine(models.Model):
+
+    _inherit = "account.analytic.line"
+
+    company_id = fields.Many2one("res.company", related=None, string='Company',
+                                 readonly=True,
+                                 default=lambda s: s.env.user.company_id.id)
