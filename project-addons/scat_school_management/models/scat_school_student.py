ed0a3c345bdb4a5127b65213581e26cbcbdd623f
Omar (Comunitea) <omar@comunitea.com>
2018-08-28 11:35:39 +0200

    [FIX] scat_school_management

--- project-addons/scat_school_management/models/scat_school_student.py
+++ project-addons/scat_school_management/models/scat_school_student.py
@@ -25,17 +25,3 @@ class scat_school_student(models.Model):
                 res={"warning":{"title":"Error", "message":"El colegio %s no tiene expediete abierto" % self.school_id.name}}
                 self.school_id=False
                 return res
-
-
-    @api.constrains('start_date', 'end_date')
-    @api.multi
-    def addcolegio(self):
-        for school in self:
-            res=self.search([('student_id', '=', school.student_id.id ),('id','!=',school.id), ('end_date', '=', False)])
-            if res:
-                raise exceptions.ValidationError("Debe finalizar el colegio anterior de este alumno %s antes de crear uno nuevo" % school.student_id.name)
-            else:
-                res=self.search([('student_id', '=', school.student_id.id ),('id','!=',school.id), ('end_date', '>', school.start_date)])
-                if res:
-                    res.write({'end_date': school.start_date})
-                    #raise exceptions.ValidationError("Debe finalizar el colegio anterior de este alumno %s antes de la fecha de inicio actual %s" % (school.student_id.name,school.start_date))
